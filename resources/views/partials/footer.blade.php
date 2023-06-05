749c3b9257625562611abf877237ff1b9d304409
MicheleBertoia2 <michelebertoia95@gmail.com>
2023-06-05 16:34:04 +0200

    [transferred] static header and footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,74 @@
 <footer>
-    FOOTER
+    <div class="f-top container">
+
+        <div class="f-left">
+          {{-- <ul v-for="(menu, index) in footerMenus" :key="index">
+            <li><a :href="menu.href"> {{ menu.title }}</a></li>
+            <li v-for="(link, index) in menu.links" :key="index"><a :href="link.href">{{ link.link }}</a></li>
+          </ul> --}}
+         <ul>
+            <li><a href="#">dc comics</a></li>
+            <li><a href="#">characters</a></li>
+            <li><a href="#">comics</a></li>
+            <li><a href="#">movies</a></li>
+            <li><a href="#">TV</a></li>
+            <li><a href="#">Games</a></li>
+            <li><a href="#">videos</a></li>
+            <li><a href="#">news</a></li>
+          </ul>
+
+          <ul>
+            <li><a href="#">shop</a></li>
+            <li><a href="#">shop DC</a></li>
+            <li><a href="#">shop DC collectibles</a></li>
+          </ul>
+
+          <ul>
+            <li><a href="#">DC</a></li>
+            <li><a href="#">terms of use</a></li>
+            <li><a href="#">privacy policy(new)</a></li>
+            <li><a href="#">ad choices</a></li>
+            <li><a href="#">advertising</a></li>
+            <li><a href="#">jobs</a></li>
+            <li><a href="#">subscriptions</a></li>
+            <li><a href="#">talent workshop</a></li>
+            <li><a href="#">CPSC certificates</a></li>
+            <li><a href="#">ratings</a></li>
+            <li><a href="#">shop help</a></li>
+            <li><a href="#">contact us</a></li>
+          </ul>
+
+          <ul>
+            <li><a href="#">sites</a></li>
+            <li><a href="#">DC</a></li>
+            <li><a href="#">MAD magazine</a></li>
+            <li><a href="#">DC kids</a></li>
+            <li><a href="#">DC universe</a></li>
+            <li><a href="#">DC power visa</a></li>
+          </ul>
+
+        </div>
+
+        <div class="f-right">
+          <img src="/img/dc-logo-bg.png" alt="">
+        </div>
+
+      </div>
+
+      <div class="f-social">
+        <div class="container">
+
+          <button>sign up now!</button>
+
+          <div class="icons">
+            <span>FOLLOW US</span>
+            <img src="/img/footer-facebook.png" alt="">
+            <img src="/img/footer-twitter.png" alt="">
+            <img src="/img/footer-youtube.png" alt="">
+            <img src="/img/footer-pinterest.png" alt="">
+            <img src="/img/footer-periscope.png" alt="">
+          </div>
+
+        </div>
+      </div>
 </footer>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,19 @@
 <header>
-    HEADER
+    <div class="top-bar container">
+        <img src="/img/dc-logo.png" alt="">
+        <nav>
+          <ul>
+            <li><a href="#">characters</a></li>
+            <li><a href="#" class="active">comics</a></li>
+            <li><a href="#">movies</a></li>
+            <li><a href="#">tv</a></li>
+            <li><a href="#">games</a></li>
+            <li><a href="#">collectibles</a></li>
+            <li><a href="#">videos</a></li>
+            <li><a href="#">fans</a></li>
+            <li><a href="#">news</a></li>
+            <li><a href="#">shop</a></li>
+          </ul>
+        </nav>
+      </div>
 </header>
