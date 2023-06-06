f256538b3a53b31e65201f344517f366b09aa62d
MicheleBertoia2 <michelebertoia95@gmail.com>
2023-06-06 17:37:16 +0200

    [minor fix]  on comic specifics styles

--- resources/views/product_detail.blade.php
+++ resources/views/product_detail.blade.php
@@ -15,7 +15,7 @@
                 <p>{{ $comic['description'] }}</p>
                 <div class="info-box d-flex  justify-content-between">
                     <span>{{ $comic['price'] }}</span>
-                    <span>disponibile</span>
+                    <span>Available</span>
                 </div>
             </div>
         </div>
@@ -24,29 +24,29 @@
             <div class="talent">
                 <h4>Talent</h4>
                 <div class="row">
-                  <div class="col-4">Art by:</div>
-                  <div class="col-8">{{ implode(",",$comic['artists'])  }}</div>
+                  <div class="col-4  lined">Art by:</div>
+                  <div class="col-8 text-center  lined">{{ implode(",",$comic['artists'])  }}</div>
                 </div>
-                <div class="row">
-                  <div class="col-4">Written by:</div>
-                  <div class="col-8">{{ implode(",",$comic['writers'])  }}</div>
+                <div class="row ">
+                  <div class="col-4  lined">Written by:</div>
+                  <div class="col-8 text-center  lined">{{ implode(",",$comic['writers'])  }}</div>
                 </div>
 
             </div>
 
             <div class="specs">
                 <h4>Specs</h4>
-                <div class="row">
-                  <div class="col-4">Series</div>
-                  <div class="col-8">{{ $comic['series'] }}</div>
+                <div class="row ">
+                  <div class="col-4  lined">Series</div>
+                  <div class="col-8 text-end  lined">{{ $comic['series'] }}</div>
                 </div>
-                <div class="row">
-                  <div class="col-4">US price:</div>
-                  <div class="col-8">{{ $comic['price'] }}</div>
+                <div class="row ">
+                  <div class="col-4  lined">US price:</div>
+                  <div class="col-8 text-end  lined">{{ $comic['price'] }}</div>
                 </div>
-                <div class="row">
-                  <div class="col-4">On Sale Date:</div>
-                  <div class="col-8">{{ $comic['sale_date'] }}</div>
+                <div class="row ">
+                  <div class="col-4  lined">On Sale Date:</div>
+                  <div class="col-8 text-end  lined">{{ $comic['sale_date'] }}</div>
                 </div>
             </div>
         </div>
